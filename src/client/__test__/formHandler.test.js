c624dfcbc64eb13d10f22c001efdda6d8e79b509
Berty <38094933+Cariloha@users.noreply.github.com>
2023-08-15 14:56:39 -0600

    feedback adjustments

--- src/client/__test__/formHandler.test.js
+++ src/client/__test__/formHandler.test.js
@@ -1,11 +1,24 @@
-import { handleSubmit } from "./src/js/formHandler.js";
+import { handleSubmit } from "./js/formHandler";
 
 describe("Testing the submit functionality", () => {
-  test("Testing the handleSubmit() function", () => {
-    const input = [];
-
-    const output = [];
-
-    expect(handleSubmit(input, "input")).toEqual(output);
+  test("Testing submit button prevent default", () => {
+    const preventDefault = jest.fn();
+    document.body.innerHTML = `<form class="form"></form>`;
+    handleSubmit();
+    const form = document.querySelector(".form");
+    form.dispatchEvent(new Event("submit", { cancelable: true }));
+    expect(preventDefault).toHaveBeenCalled();
+  });
+  test("Test when the input is blank", () => {
+    document.body.innerHTML = `
+      <form class="form"></form>
+      <input id="text" value="" />
+      <div id="errorMessage"></div>
+    `;
+    handleSubmit();
+    const form = document.querySelector(".form");
+    form.dispatchEvent(new Event("submit"));
+    const errorMessage = document.getElementById("errorMessage");
+    expect(errorMessage.textContent).toBe("Input cannot be blank!");
   });
 });
--- src/client/__test__/namechecker.test.js
+++ src/client/__test__/namechecker.test.js
@@ -1,11 +1,17 @@
-import { handleSubmit } from "./src/js/nameChecker.js";
+import { checkForName } from "./js/nameChecker";
 
 describe("Testing name checker functionality", () => {
+  global.alert = jest.fn();
   test("Testing the checkForName() function", () => {
-    const input = [];
-
-    const output = [];
-
-    expect(handleSubmit(input, "input")).toEqual(output);
+    const input = "Picard";
+    global.alert.mockClear();
+    checkForName(input);
+    expect(global.alert).toEqual("Welcome, Captain");
+  });
+  test("Testing the checkFornName() function when invalid name", () => {
+    const input = "NotValidName";
+    global.alert.mockClear();
+    checkForName(input);
+    expect(global.alert).not.toEqual();
   });
 });
